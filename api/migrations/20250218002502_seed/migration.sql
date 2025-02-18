88bc74b87e8b5a8a6eb910ab164d7b36f8c112ab
DarioJunior <dario.junior.rs@gmail.com>
2025-02-18 09:49:40 -0300

    feat: add user repository

--- api/migrations/20250218002502_seed/migration.sql
+++ api/migrations/20250218002502_seed/migration.sql
@@ -9,9 +9,11 @@ INSERT INTO public."User" (email,name,"password") VALUES
 	 ('usuario2@email.com','Usuário 2','User@2');
 
 INSERT INTO public."Task" (name,solved,scheduled_for,"createdAt","updatedAt","deletedAt","createdBy") VALUES
-	 ('Task 1 - Usuário 1',false,'2025-02-25 21:53:03.302','2025-02-17 21:53:32.886','2025-02-17 21:53:03.302',NULL,1);
-   INSERT INTO public."Task" (name,solved,scheduled_for,"createdAt","updatedAt","deletedAt","createdBy") VALUES
-	 ('Task 2 - Usuário 1',false,'2025-02-25 21:53:03.302','2025-02-17 21:53:32.886','2025-02-17 21:53:03.302',NULL,1);
-   INSERT INTO public."Task" (name,solved,scheduled_for,"createdAt","updatedAt","deletedAt","createdBy") VALUES
-	 ('Task 1 - Usuário 2',false,'2025-02-25 21:53:03.302','2025-02-17 21:53:32.886','2025-02-17 21:53:03.302',NULL,2);
+('Task 1 - Usuário 1',false,'2025-02-25 21:53:03.302','2025-02-17 21:53:32.886','2025-02-17 21:53:03.302',NULL,1);
+
+INSERT INTO public."Task" (name,solved,scheduled_for,"createdAt","updatedAt","deletedAt","createdBy") VALUES
+('Task 2 - Usuário 1',false,'2025-02-25 21:53:03.302','2025-02-17 21:53:32.886','2025-02-17 21:53:03.302',NULL,1);
+
+INSERT INTO public."Task" (name,solved,scheduled_for,"createdAt","updatedAt","deletedAt","createdBy") VALUES
+('Task 1 - Usuário 2',false,'2025-02-25 21:53:03.302','2025-02-17 21:53:32.886','2025-02-17 21:53:03.302',NULL,2);
 
